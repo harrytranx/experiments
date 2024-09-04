c7b86ce35e2d3e83552bf2f75115f0bd1c8b0021
Harry Tran <tranx@meta.com>
2024-09-04 15:46:45 +0000

    update lib

---
+++ lib/eval_helper.py
@@ -0,0 +1,314 @@
+import os
+import re
+import pandas as pd
+import json
+from typing import Optional, List, Dict, Any
+import glob
+import copy
+
+from lib import utils
+from lib.slurm import run_sbatch_job, get_log_file
+
+ALL_BENCHMARKS = [
+    "mmmu",
+    "docvqa",
+    "mathvista",
+    "ai2d",
+    "chartqa",
+    "vqa",
+    "textvqa",
+    "infographics_w_ocr",
+    "infographics",
+    "mmbench"
+]
+
+REPORT_BENCHMARKS = {
+    'mmmu/mllm_eval_accuracy': 'mmmu_v2',
+    'mmmu/accuracy': 'mmmu_v1',
+    'docvqa/anls_total_score': 'docvqa',
+    'mathvista/accuracy': 'mathvista',
+    'ai2d/accuracy': 'ai2d',
+    'chartqa/accuracy': 'chartqa',
+    'vqa/accuracy': 'vqa',
+    'textvqa/accuracy': 'textvqa',
+    'infographics_w_ocr/anls_total_score': 'infographics_w_ocr',
+    'infographics/anls_total_score': 'infographics',
+    'mmbench/overall': 'mmbench'
+}
+
+
+def get_report_benchmarks(all_benchmark_df: pd.DataFrame) -> pd.DataFrame:
+    df = all_benchmark_df[list(REPORT_BENCHMARKS.keys())]
+    df = df.rename(columns=REPORT_BENCHMARKS)
+    df['textvqa'] = round(df['textvqa']/100, 4)
+    return df
+
+
+def run_eval_plan(
+    eval_base_sbatch: str,
+    aligner_parent_dir: str,
+    eval_config_dir: str,
+    checkpoint_dir: str,
+    checkpoints: list[int],
+    save_eval_jobs: str,
+    benchmarks: Optional[List[str]] = None,
+    rerun_if_exists: bool = False,
+    print_cmd: bool = False,
+    print_job_dict: bool = False
+):
+
+    job_dict = {}
+
+    if os.path.exists(save_eval_jobs) and not rerun_if_exists:
+        raise RuntimeError(f"Found existing eval_jobs at: {save_eval_jobs}")
+
+    if benchmarks is None:
+        benchmarks = ALL_BENCHMARKS
+
+    for benchmark in benchmarks:
+        job_dict[benchmark] = {}
+
+        for chk in checkpoints:
+            params = {
+                "aligner_parent_dir": aligner_parent_dir,
+                "json_config": f"{eval_config_dir}/eval_{benchmark}.json",
+                "checkpoint_dir": checkpoint_dir,
+                "benchmark_name": benchmark,
+                "checkpoint_id": str(chk)
+            }
+
+            assert os.path.exists(params["json_config"])
+            assert os.path.exists(
+                f"{params['checkpoint_dir']}/checkpoint-{chk}")
+
+            job_id = run_sbatch_job(
+                sbatch_base_script=eval_base_sbatch,
+                sbatch_overwrite={
+                    "job-name": f"eval_{benchmark}"
+                },
+                positional_env_vars=list(params.values()),
+                print_cmd=print_cmd
+            )
+
+            job_dict[benchmark][chk] = int(job_id)
+            
+    if print_job_dict:
+        print(job_dict)
+
+    os.makedirs(os.path.dirname(save_eval_jobs), exist_ok=True)
+    with open(save_eval_jobs, 'w') as f:
+        json.dump(job_dict, f, indent=4)
+
+    print(f"eval jobs saved to: {save_eval_jobs}")
+
+
+def extract_values(filename):
+    if not os.path.exists(filename):
+        return None
+
+    # Define the pattern to match the line and capture the required parts
+    pattern = r"Writing (\S+) with value ([\d\.]+) to TensorBoard"
+
+    # List to hold the extracted values
+    extracted_values = []
+
+    # Open the file and read line by line
+    with open(filename, 'r', encoding='utf-8', errors='replace') as file:
+        # with open(filename, 'r') as file:
+        for line in file:
+            # Search for the pattern in each line
+            match = re.search(pattern, line)
+            if match:
+                # Extract the path and the value
+                path = match.group(1)
+                value = float(match.group(2))
+                extracted_values.append((path, value))
+
+    # if len(extracted_values) == 0:
+    #     return None
+
+    return extracted_values
+
+
+# def get_eval_scores(job_dict, output_csv=None) -> pd.DataFrame:
+def get_eval_scores(checkpoint_dir: str, checkpoint_id: int, output_csv=None) -> pd.DataFrame:
+    results = {}
+    
+    job_dict_file = get_eval_jobs_record(checkpoint_dir, checkpoint_id)
+    with open(job_dict_file, 'r') as f:
+        job_dict = json.load(f)
+    print(job_dict)
+    for b in job_dict:
+        results[b] = {}
+        for chk in job_dict[b]:
+            job_id = job_dict[b][chk]
+            # log = f"/fsx_0/user/tranx/output/slurm_logs/output_{job_id}.txt"
+            # print(log)
+            log = get_log_file(int(job_id))
+
+            res = extract_values(log)
+            if res:
+                print(f"Got result for {b} - {chk}: {res}")
+                results[b][chk] = res
+
+    scores = {
+        "mmmu": ["accuracy", "mllm_eval_accuracy"],
+        "docvqa": ["anls_total_score", "mllm_evaluation_anls_score"],
+        "mathvista": ["accuracy"],
+        "ai2d": ["accuracy"],
+        "chartqa": ["accuracy"],
+        "vqa": ["accuracy", "mllm_evaluation_accuracy"],
+        "textvqa": ["accuracy", "mllm_eval_accuracy"],
+        "infographics_w_ocr": ["anls_total_score", "mllm_evaluation_anls_score"],
+        "infographics": ["anls_total_score", "mllm_evaluation_anls_score"],
+        "mmbench": ["overall"]
+    }
+
+    component_scores = []
+    for k, v in scores.items():
+        for vi in v:
+            component_scores.append(f"{k}/{vi}")
+
+    df = pd.DataFrame(columns=component_scores)
+
+    for b in results:
+        for chk in results[b]:
+            res = results[b][chk]
+            # print(b, chk, results[b][chk])
+            for x in res:
+                component, val = x[0], x[1]
+                if component in component_scores:
+                    df.loc[chk, component] = round(val, 4)
+
+    df = get_report_benchmarks(df)
+
+    if output_csv is not None:
+        df.to_csv(output_csv, index=False)
+
+    return df
+
+
+def get_eval_scores_all(checkpoint_dir: str) -> pd.DataFrame:
+    checkpoints = get_checkpoints(checkpoint_dir)
+    print(checkpoints)
+    df = pd.DataFrame()
+    
+    for c in checkpoints:
+        try:
+            df_c = get_eval_scores(checkpoint_dir, c)
+            df = pd.concat([df, df_c])     
+            print(c)   
+        except Exception:
+            pass
+        
+    return df
+
+
+def get_checkpoints(output_dir: str) -> List[int]:
+    """
+    List all checkpoints in a given output folder
+    """
+    checkpoint_dirs = glob.glob(os.path.join(output_dir, '*/'))
+    checkpoints = []
+
+    for d in checkpoint_dirs:
+        checkpoint_name = d.split('/')[-2]  # e.g. checkpoint-1800
+        if checkpoint_name.startswith('checkpoint'):
+            checkpoint_number = int(checkpoint_name.split('-')[-1])  # 800
+            checkpoints.append(checkpoint_number)
+
+    checkpoints = sorted(checkpoints)
+
+    return checkpoints
+
+
+def get_checkpoints_eval(output_dir: str) -> List[int]:
+    """
+    List all checkpoints with eval jobs
+    """
+    checkpoints = []
+    for file in glob.glob(f"{output_dir}/evals/*.json"):
+        c = file.replace(".json", "").split("-")[-1]
+        checkpoints.append(int(c))
+    
+    checkpoints = sorted(checkpoints)
+    
+    return checkpoints
+
+
+def get_eval_jobs_record(checkpoint_dir: str, checkpoint_id: int):
+    return f"{checkpoint_dir}/evals/eval_jobs_checkpoint-{checkpoint_id}.json"
+    
+    
+def run_eval_sweep(output_dir: str, eval_sbatch: str, eval_config_dir: str, aligner_parent_dir:str, print_cmd:bool = False, min_checkpoint: int=0):
+    """
+    Start eval sweep on new checkpoints
+    """
+    
+    checkpoints = get_checkpoints(output_dir)
+    checkpoints_eval = get_checkpoints_eval(output_dir)
+    
+    new_checkpoints = [int(c) for c in checkpoints if c not in checkpoints_eval]
+    new_checkpoints = sorted(new_checkpoints)
+    
+    print(f"New checkpoints: {new_checkpoints}")
+    
+    for c in new_checkpoints:
+        if c < min_checkpoint:
+            continue 
+        
+        if c not in checkpoints_eval:
+            print(f"Start eval for {output_dir}/checkpoint-{c}")
+            
+            run_eval_plan(
+                eval_base_sbatch=eval_sbatch,
+                aligner_parent_dir=aligner_parent_dir,
+                eval_config_dir=eval_config_dir,
+                checkpoint_dir=output_dir,
+                checkpoints=[c],
+                # save_eval_jobs=f"{output_dir}/evals/eval_jobs_checkpoint-{c}.json"
+                save_eval_jobs=get_eval_jobs_record(output_dir, c),
+                print_cmd=print_cmd
+            )
+
+
+def get_eval_config_overwrite(train_config: Dict[str, Any], eval_config: Dict[str, Any]) -> Dict[str, Any]:
+    delete_keys = []
+    for k in train_config["trainer_args"]:
+        if k not in eval_config["eval_args"]:
+            delete_keys.append(k)
+
+
+    update_keys = {}
+    for k, v in eval_config["eval_args"].items():
+        if (k not in train_config["trainer_args"]) or (v != train_config["trainer_args"][k]):
+            update_keys[k] = v
+            
+    overwrite = {
+        "delete": delete_keys,
+        "update": update_keys
+    }
+    
+    return overwrite
+
+
+def gen_eval_config(train_config, overwrite):
+    
+    eval_config = {
+        "eval_args": copy.deepcopy(train_config["trainer_args"]),
+        "fsdp_config": copy.deepcopy(train_config["fsdp_config"])
+    }
+    
+    for k in overwrite["delete"]:
+        if k in eval_config["eval_args"]:
+            eval_config["eval_args"].pop(k)
+            
+    eval_config["eval_args"].update(overwrite["update"])
+    
+    eval_config["eval_args"].update({
+        "checkpoints_perception_tokenizer": train_config["trainer_args"]["checkpoints_perception_tokenizer"],
+        "lora_checkpoint": "CHECKPOINT_PATH",
+        "lora_tokenizer_checkpoint": "CHECKPOINT_PATH/adapter_tokenizer"
+    })
+    
+    return eval_config
---
+++ lib/slurm.py
@@ -0,0 +1,349 @@
+import pandas as pd
+from typing import Optional, Dict, Any, List
+import json
+import re
+from pprint import pprint
+import os
+
+from lib import utils
+
+HOST_PREFIX="h100-st-p548xlarge"
+FSX_MAP = {
+    "/opt/hpcaas/.mounts/fs-06bc3d6b93146dddd": "fsx_0",
+    "/opt/hpcaas/.mounts/fs-04d11daf19d159145": "fsx_1",
+    "/opt/hpcaas/.mounts/fs-0d26265fcbf46521a": "fsx_2",
+    "/opt/hpcaas/.mounts/fs-07a3112c6c962d053": "fsx_3"
+}
+
+def run_sbatch_job(sbatch_base_script, sbatch_overwrite, positional_env_vars, print_cmd=False):
+    """
+    sbatch_base = "/fsx_0/user/tranx/experiments/eval/sbash_eval.sh"
+    sbatch_overwrite = {
+        "job-name": "eval",
+    }
+
+    positional_env_vars = [1, 2, 3, 4]
+    """
+
+    sbatch_vars_string = []
+    for k, v in sbatch_overwrite.items():
+        sbatch_vars_string.append(f"--{k}={v}")
+    sbatch_vars_string = ' '.join(sbatch_vars_string)
+
+    positional_env_string = " ".join([str(x) for x in positional_env_vars])
+
+    cmd = f"sbatch --parsable {sbatch_vars_string} {sbatch_base_script} {positional_env_string}"
+    if print_cmd:
+        print(cmd)
+
+    job_id = utils.get_bash_output(cmd, print_output=False)
+    job_id = int(job_id)
+
+    return job_id
+
+
+def get_log_file(job_id: int):
+    return f"/fsx_0/user/tranx/slurm_logs/output_{job_id}.txt"
+
+
+class SlurmClient():
+    def __init__(self):
+        pass
+
+    @property
+    def num_nodes(self):
+        """
+        Return total number of nodes in the cluster
+        """
+        info = self.get_info()
+        count = 0
+        for v in info.values():
+            count += v[0]
+
+        return count
+
+    def get_info(self) -> Optional[pd.DataFrame]:
+        """
+        Return node info as a dictionary
+        {
+            state: [count, node_list]
+        }
+        """
+
+        output = utils.get_bash_output("sinfo --format=%t\t%D\t%N")
+
+        if output is None:
+            raise RuntimeError("Unable to get SLURM info")
+
+        return utils.bash_output_to_table(output)
+
+    def get_nodes_by_state(self, state: str) -> List[str]:
+        """
+        Get list of nodes given state
+        """
+        df = self.get_info()
+
+        try:
+            node_list_expression = df[df.STATE == state].NODELIST.item()
+            node_list_expression = node_list_expression.replace(
+                f"{HOST_PREFIX}-", "")
+
+            node_list = utils.list_expression_to_list(node_list_expression)
+
+            return node_list
+        except Exception as e:
+            print(e)
+            return None
+
+    def get_queue_summary(self):
+        q = self.get_queue()
+        q_summary = q.groupby(['ACCOUNT', 'ST']).aggregate(
+            {'NODES': 'sum'}).reset_index()
+
+        return q_summary
+
+    def get_recent_queue(self, account=None):
+        q = self.get_queue()
+        q = q[q.ST == 'R']
+        q['TIME_SECS'] = q['TIME'].apply(
+            lambda x: utils.get_elasped_seconds(x))
+
+        if account is not None:
+            q = q[q['ACCOUNT'] == account]
+
+        q = q.sort_values(by='TIME_SECS', ascending=True)
+
+        return q
+
+    def get_queue(self) -> Optional[pd.DataFrame]:
+        """
+        Get SLURM queue into a dictionary
+        """
+
+        output = utils.get_bash_output(
+            "squeue --format=%i\t%a\t%j\t%u\t%t\t%S\t%M\t%D")
+        if output is None:
+            raise RuntimeError("Unable to get SLURM queue")
+
+        queue = utils.bash_output_to_table(output)
+        queue['NODES'] = queue['NODES'].astype(int)
+
+        return queue
+
+    def get_job_hist_json(self, start_date: str) -> List[Dict[str, Any]]:
+        """
+        Get Slurm job history since a start_date (e.g., "2024-07-01")
+        """
+        cmd = f"sacct -a --starttime {start_date} --json"
+        output = utils.get_bash_output(cmd)
+
+        output_dict = json.loads(output)
+
+        jobs = output_dict["jobs"]
+
+        return jobs
+    
+    def get_job_launch_info(self, job_id):
+        cmd = f"sacct -a -j {job_id} --json"
+        output = utils.get_bash_output(cmd)
+        output_dict = json.loads(output)
+        
+        result = {
+            "working_directory": output_dict["jobs"][0]["working_directory"],
+            "submit_line": output_dict["jobs"][0]["submit_line"]
+        }
+        
+        return result
+
+    def get_job_launch_cmd(self, job_id):
+        launch_info = self.get_job_launch_info(job_id)
+        working_dir = launch_info['working_directory']
+        for k, v in FSX_MAP.items():
+            working_dir = working_dir.replace(k, v)
+            
+        cmd = [
+            f"cd {working_dir}",
+            launch_info["submit_line"]
+        ]
+
+        print(cmd[0])
+        print(cmd[1])
+
+    def get_job_hist_table(self, start_date: str) -> Optional[pd.DataFrame]:
+        """
+        Get Slurm job history since a start_date (e.g., "2024-07-01")
+        """
+
+        # run bash command to get job history output, then convert to df
+        bash_command = f"sacct -a --starttime {start_date} --format=User,JobID,Jobname%50,partition,state,time,start,end,elapsed,nnodes,ncpus,nodelist"
+        output = utils.get_bash_output(bash_command)
+        lines = output.split("\n")
+
+        header = lines[0].split()
+        num_cols = len(header)
+
+        data = [line.split() for line in lines[2:-1]]
+
+        data = [row for row in data if len(row) == num_cols]
+
+        df = pd.DataFrame(columns=header, data=data)
+
+        # post processing
+        df['NNodes'] = df['NNodes'].astype(int)
+        df['NCPUS'] = df['NCPUS'].astype(int)
+        df['Elapsed_hr'] = df['Elapsed'].map(utils.get_elapsed_hours)
+
+        return df
+
+    def get_qos_partitions(self):
+        # output = utils.get_bash_output("sacctmgr show qos format=name%40,GrpTRES%40,priority")  
+        output = utils.get_bash_output("sacctmgr show qos format=name%40\tGrpTRES%40\tpriority")   
+           
+        if output is None:
+            raise RuntimeError("Unable to get SLURM QOS")
+
+        print(output)
+
+
+class LogProcessor:
+    @staticmethod
+    def parse_tres_count(tres_list):
+        tres = {}
+        for item in tres_list:
+            tres[item["type"]] = item["count"]
+
+        return tres
+
+    @staticmethod
+    def parse_sbatch_config(sbatch_script_file: str, job_id: int):
+        config = {}
+
+        lines = utils.read_file_lines(sbatch_script_file)
+        if lines is not None:
+            for line in lines:
+                line = line.strip()
+                if line.startswith("#SBATCH"):
+                    # ex: #SBATCH --job-name=vitg_2
+                    line = line.replace("%j", str(job_id))
+                    line = line.split()[-1]  # --job-name=vitg_2
+                    line = line[2:]  # job-name=vitg_2
+                    if '=' in line:
+                        arg, value = line.split('=')
+                        if value.isdigit():
+                            value = int(value)
+
+                        config[arg] = value
+
+        return config
+
+    @staticmethod
+    def parse_job_error_log(log_file: str):
+        """
+        Parse error log file to identify error pattern and training progress
+        Supported jobs:
+            - vision encoder: /fsx_0/user/jiazhi/logs/
+        """
+        log_info = {
+            "train_epoch": None,
+            "found_error_patterns": []
+        }
+
+        # known error patterns
+        error_patterns = [
+            {
+                "catch_phrase": "RendezvousTimeoutError",
+                "note": "usually happens during launch, timeout after 10min"
+
+            },
+            {
+                "catch_phrase": "NCCL  NET/OFI Request error",
+                "note": ""
+            },
+            {
+                "catch_phrase": "Watchdog caught collective operation timeout",
+                "note": "followed by WorkNCCL..."
+            },
+            {
+                "catch_phrase": "uncorrectable ECC error encountered",
+                "note": "GPU error"
+            }
+        ]
+
+        lines = utils.read_file_lines(log_file)
+        if lines is not None:
+            # read backward to find max train epoch reached
+            for line in lines[::-1]:
+                match = re.search(r"Train Epoch: (\d+)", line)
+                if match:
+                    log_info['train_epoch'] = int(match.group(1))
+                    break
+
+            # look for error patterns
+            for line in lines:
+                for p in error_patterns:
+                    if (p["catch_phrase"] not in log_info["found_error_patterns"]):
+                        if re.search(p["catch_phrase"], line):
+                            log_info["found_error_patterns"].append(
+                                p["catch_phrase"])
+
+        return log_info
+
+    @staticmethod
+    def parse_job_info(info):
+        """
+        parse job info json
+        """
+
+        if not info['submit_line'].startswith("sbatch"):
+            return
+
+        if info['qos'] != 'ar-ai-hipri':
+            return
+
+        allocated_tres = LogProcessor.parse_tres_count(
+            info["tres"]["allocated"])
+        node_count = allocated_tres.get("node", None)
+        if node_count is None or node_count < 10:
+            return
+
+        user = info['association']['user']
+        job_id = info['job_id']
+
+        if user not in ('jiazhi', 'tranx', 'qyh', 'samueldo'):
+            return
+
+        state = info['state']['current']
+        if state == "CANCELLED":
+            return
+
+        # process sbatch file
+        sbatch_file = info['submit_line'].split()[-1]
+        sbatch_file = os.path.join(info['working_directory'], sbatch_file)
+        config = LogProcessor.parse_sbatch_config(sbatch_file, info['job_id'])
+
+        # print('-------')
+        # print("job_id:", job_id)
+        # print("node_count:", node_count)
+        # print("user:", user)
+        # print("config:", config)
+
+        log_info = None
+        if 'error' in config and os.path.exists(config['error']):
+            log_info = LogProcessor.parse_job_error_log(config['error'])
+
+        output = {
+            "job_id": job_id,
+            "node_count": node_count,
+            "user": user,
+            "state": state,
+            "log_info": log_info,
+            "start_time": utils.timestamp_to_str(info['time']['start']),
+            "end_time": utils.timestamp_to_str(info['time']['end']) if state != "RUNNING" else None,
+            "elapsed_seconds": info['time']['elapsed'],
+            "sbatch_config": config
+        }
+
+        print('-------')
+        pprint(output)
+
+        return output
