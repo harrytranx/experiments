5539dcf8a87d83ecd8fbefd6a019e5a7db8ca94a
Harry Tran <tranx@meta.com>
2024-10-01 13:55:42 +0000

    update notebooks

--- eval_runner/eval_runner.py
+++ eval_runner/eval_runner.py
@@ -20,24 +20,29 @@ def main():
     ev_master = eval_helper.EvalHelper(
         code_dir="/fsx_0/user/tranx/rsync",
         eval_sbatch="/fsx_0/user/tranx/rsync/llm_mm_aligner/experiments/aws/launch_eval_sbatch.sh",
-        eval_config_dir="/fsx_0/user/tranx/rsync/llm_mm_aligner/experiments/aws/mm10/stage1/eval_overwrite"
+        eval_config_dir="/fsx_0/user/tranx/rsync/llm_mm_aligner/experiments/aws/mm10/stage2/eval_overwrite"
     )
     
-    OUTPUT_S1_norm_r3 = "/fsx_0/checkpoints/mm10/MM10-Stage1-70B/MH21_70B_224px_norm_R3"
+    # watch_path = "/fsx_0/checkpoints/mm10/MM10-Stage1-70B/MH21_70B_224px_norm_R3"
+    watch_paths = [
+        "/fsx_0/checkpoints/mm10/MM10-Stage2-70B/MH21_70B_336px_exp32a",
+        "/fsx_0/checkpoints/mm10/MM10-Stage2-70B/MH21_70B_336px_exp32a_bz48"
+    ]
     
     while True:  
-        ts = utils.get_local_time()
-        print(ts)
-        
-        ev_master.run_eval_sweep(
-            checkpoint_dir=OUTPUT_S1_norm_r3,
-            benchmarks=["mmmu"],
-            # benchmarks=eval_helper.ALL_BENCHMARKS,
-            slurm_qos="midpri",
-            # checkpoints=[100],
-            update_if_exists=False,
-            print_cmd=True
-        )
+        for path in watch_paths:
+            ts = utils.get_local_time()
+            print(f"[{ts}] scanning for new checkpoints in: {path}")
+            
+            ev_master.run_eval_sweep(
+                checkpoint_dir=path,
+                # benchmarks=["mmmu"],
+                benchmarks=eval_helper.ALL_BENCHMARKS,
+                slurm_qos="midpri",
+                # checkpoints=[100],
+                update_if_exists=False,
+                print_cmd=True
+            )
         
         time.sleep(300) 
         
